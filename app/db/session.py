a02a24ad16388c00c391837ab5c2e541011b5586
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-03 02:54:02 +0330

    Create session.py
    Add database brain in file

---
+++ app/db/session.py
@@ -0,0 +1,24 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from typing import AsyncGenerator
+from app.core.config import settings
+
+
+engine = create_async_engine(settings.DATABASE_URL, echo=True)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with SessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
