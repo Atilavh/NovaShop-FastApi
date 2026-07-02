3129c6711eb724cfe2a3ced9c106827dc337e013
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-03 02:54:51 +0330

    Create user models.py
    Add user model and user_role class to manage status of user

---
+++ app/users/model/models.py
@@ -0,0 +1,26 @@
+import uuid
+import enum
+from datetime import datetime
+from sqlalchemy import String, Boolean, Enum, func, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.dialects.postgresql import UUID
+from app.utils.time_stamp_for_model import TimestampMixin
+from app.db.base import Base
+from app.auth.model.models import OTP
+
+
+class UserRole(str, enum.Enum):
+    CUSTOMER = "customer"
+    SELLER = "seller"
+    ADMIN = "admin"
+
+class User(Base, TimestampMixin):
+    __tablename__ = "users"
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    first_name: Mapped[str] = mapped_column(String(30), nullable=True, default='')
+    last_name: Mapped[str] = mapped_column(String(30), nullable=True, default='')
+    phone_number: Mapped[str] = mapped_column(nullable=False, unique=True)
+    role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.CUSTOMER, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=False)
+    last_login: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    otps = relationship(OTP, back_populates="user")
