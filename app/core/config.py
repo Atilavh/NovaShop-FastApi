d90d60866d480ad6e96ef0f328525ab6862b083a
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-05 02:25:03 +0330

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -2,9 +2,16 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional
 
 class Settings(BaseSettings):
+    # db configs
     DATABASE_URL: str
     DATABASE_SYNC_URL: str
 
+    # jwt configs
+    SECRET_KEY: str
+    ALGORITHM: str = "HS256"  
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    
     model_config = SettingsConfigDict(env_file='.env', extra="ignore")
 
 
