740b48d0a3ed2f5d0f3b8b1a1e9a2e1d6eee62a6
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-05 02:22:49 +0330

    Update auth_service.py
    Add get_current_user func to real auth and depends for the required functions

--- app/auth/services/auth_service.py
+++ app/auth/services/auth_service.py
@@ -1,10 +1,14 @@
-from app.auth.schemas.auth_schema import UserRegisterSchema, UserLoginSchema, UserVerifiedSchema
+from app.auth.schemas.auth_schema import UserRegisterSchema, UserLoginSchema
+from app.db.session import get_db
+from app.auth.services.jwt_service import verify_token
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.users.model.models import User
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 
+security = HTTPBearer()
 
 
 
@@ -42,3 +46,44 @@ async def user_login(db: AsyncSession, request: UserLoginSchema):
     existing_user.is_active = True
     await db.commit()
     return existing_user
+
+
+
+async def get_user_from_db(db: AsyncSession, user_id):
+    stmt = select(User).where(User.id == user_id)
+    result = await db.execute(stmt)
+    user = result.scalar_one_or_none()
+
+    return user
+
+
+
+async def get_current_user(auth: HTTPAuthorizationCredentials = Depends(security), db: AsyncSession = Depends(get_db)) -> User:
+    token = auth.credentials
+    payload = verify_token(token, "access")
+
+    if payload is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User ID not found in token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user = await get_user_from_db(db, user_id)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found or account disabled",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        
+    return user
+
