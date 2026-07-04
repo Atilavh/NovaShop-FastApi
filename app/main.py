1760bcadd1cb03cba99ab6e5a05496f0ef92aa49
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-05 02:25:10 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from app.auth.routes.api.v1.auth_api import router
+from app.auth.routes.api.v1.auth_api import router as auth_router
+from app.users.routes.api.v1.users_api import router as user_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,4 +11,5 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(router, prefix="/api/v1/auth")
+app.include_router(auth_router, prefix="/api/v1/auth")
+app.include_router(user_router, prefix="/api/v1/user")
