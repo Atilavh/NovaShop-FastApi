b4e6c7345ecff16bf4a30adeebc79a2c5ee9e861
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-05 02:20:16 +0330

    Update auth_api.py
    Complete all auth process with jwt token

--- app/auth/routes/api/v1/auth_api.py
+++ app/auth/routes/api/v1/auth_api.py
@@ -1,8 +1,16 @@
+import uuid
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.session import get_db
 from app.auth.services.auth_service import user_register, user_login
+from app.auth.services.jwt_service import (
+    create_access_token,
+    create_refresh_token,
+    verify_token,
+)
+from app.utils.refresh_token_logic import RefreshTokenRepository
+from datetime import datetime, timezone
 from app.auth.schemas.auth_schema import UserRegisterSchema, UserLoginSchema, UserVerifiedSchema
 from app.auth.services.otp_service import generate_otp, verified_otp
 from app.db.redis import redis
@@ -34,17 +42,108 @@ async def login(request: UserLoginSchema, db: AsyncSession = Depends(get_db)):
       "otp": otp
    }
 
-@router.post('/vreified')
+@router.post("/verified")
 async def verified(request: UserVerifiedSchema, db: AsyncSession = Depends(get_db)):
-   result = await verified_otp(phone_number=request.phone_number, input_otp=request.otp, redis=redis, db=db)
-   if not result:
-      return {
-         "error": True,
-         "message": "otp invalid or expired",
-      }
-   return {
-      "error": False,
-      "message": "login successfully, Welcome",
-      "access_token":"test",
-      "refresh_token":"test"
-   }
+    
+    user = await verified_otp(
+        phone_number=request.phone_number,
+        input_otp=request.otp,
+        redis=redis,
+        db=db,
+    )
+
+    if not user:
+        return {
+            "error": True,
+            "message": "otp invalid or expired",
+        }
+
+    access_token = create_access_token(user)
+    refresh_token = create_refresh_token(user)
+
+    payload = verify_token(refresh_token, "refresh")
+
+    await RefreshTokenRepository.create_refresh(
+        db=db,
+        user_id=user,
+        jti=str(uuid.uuid4()),
+        expires_at=datetime.fromtimestamp(
+            payload["exp"],
+            tz=timezone.utc,
+        ),
+    )
+
+    await db.commit()
+
+    return {
+        "error": False,
+        "message": "login successfully, Welcome",
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+    }
+
+@router.post("/logout")
+async def logout(refresh_token: str, db: AsyncSession = Depends(get_db)):
+    
+    payload = verify_token(refresh_token, "refresh")
+
+    if payload is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid refresh token",
+        )
+
+    refresh = await RefreshTokenRepository.get_refresh_by_jti(
+        db,
+        payload["jti"],
+    )
+
+    if refresh:
+        await RefreshTokenRepository.revoke_refresh(refresh)
+        await db.commit()
+
+    return {
+        "message": "Logged out successfully",
+    }
+
+
+@router.post("/refresh")
+async def refresh(token: str, db: AsyncSession = Depends(get_db)):
+    
+    payload = verify_token(token, "refresh")
+
+    if payload is None:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid refresh token",
+        )
+
+    refresh = await RefreshTokenRepository.get_refresh_by_jti(
+        db,
+        payload["jti"],
+    )
+
+    if refresh is None or refresh.is_revoked:
+        raise HTTPException(
+            status_code=401,
+            detail="Refresh token revoked",
+        )
+    refresh.is_revoked = True
+
+    await RefreshTokenRepository.create_refresh(
+        db=db,
+        user_id=payload["sub"],
+        jti=str(uuid.uuid4()),
+        expires_at=datetime.fromtimestamp(
+            payload["exp"],
+            tz=timezone.utc,
+        ),
+    )
+
+    await db.commit()
+    access_token = create_access_token(payload["sub"])
+    refresh_token = create_refresh_token(payload["sub"])
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token
+    }
