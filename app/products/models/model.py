dbb966e20351b8afbae3942aa1b801447ec17508
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-08 00:37:09 +0330

    Create product model.py
    Add all product and dependencies

---
+++ app/products/models/model.py
@@ -0,0 +1,65 @@
+import uuid
+import enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Integer, Enum, Text, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from datetime import datetime
+from app.db.base import Base
+from app.utils.time_stamp_for_model import TimestampMixin
+
+class ProductStatus(str, enum.Enum):
+    ACTIVE = "active"
+    DRAFT = "draft"
+    OUT_OF_STOCK = "out_of_stock"
+
+class Product(Base, TimestampMixin):
+    __tablename__ = "products"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    slug: Mapped[str] = mapped_column(String(255), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    price: Mapped[int] = mapped_column(Integer, nullable=False)
+    stock: Mapped[int] = mapped_column(Integer, default=0)
+    status: Mapped[ProductStatus] = mapped_column(Enum(ProductStatus), default=ProductStatus.DRAFT)
+
+    seller_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"), nullable=True)
+    seller = relationship("User", back_populates="products")
+    category_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("categories.id"),
+        nullable=False
+    )
+
+    category = relationship(
+        "Category",
+        back_populates="products"
+    )
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    slug: Mapped[str] = mapped_column(String(255), nullable=False)
+    parent_id: Mapped[uuid.UUID | None] = mapped_column(
+        ForeignKey("categories.id"),
+        nullable=True
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    parent = relationship(
+        "Category",
+        remote_side="Category.id",
+        back_populates="children"
+    )
+    children = relationship(
+        "Category",
+        back_populates="parent"
+    )
+    products = relationship(
+        "Product",
+        back_populates="category"
+    )
