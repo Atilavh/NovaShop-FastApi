8124ff4957e0a25142c214a8fc292c7a4c551500
Atila <161023142+Atilavh@users.noreply.github.com>
2026-07-19 00:59:13 +0330

    Create locustfile.py

---
+++ app/utils/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser, task, between
+
+
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def get_products(self):
+        self.client.get("/api/v1/")
